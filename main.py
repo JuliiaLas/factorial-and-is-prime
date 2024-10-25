b9e232b6d58804f8ea2f34ae90199832e555f706
Yuliya <lashkarbekovayuliya@gmail.com>
2024-10-25 15:04:15 +0500

    check

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ def factorial(n):
     else:
         return n * factorial(n-1)
 
-def is_prime(num):
-    if num <= 1:
-        return False
-    for i in range(2, int(num**0.5) + 1):
-        if num % i == 0:
-            return False
-    return True 1
+# def is_prime(num):
+#     if num <= 1:
+#         return False
+#     for i in range(2, int(num**0.5) + 1):
+#         if num % i == 0:
+#             return False
+#     return True 1
